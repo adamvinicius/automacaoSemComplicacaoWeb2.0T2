c59d67a96f1bd00eec39bb3156b3818e4a134eea
adamvinicius <adamvinicius@gmail.com>
2021-09-25 12:37:02 -0300

    aula 10 pageFactory

--- src/test/java/br/com/chronosacademy/pages/CursosPage.java
+++ src/test/java/br/com/chronosacademy/pages/CursosPage.java
@@ -1,19 +1,22 @@
 package br.com.chronosacademy.pages;
 
+import br.com.chronosacademy.maps.CursoMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class CursosPage {
     WebDriver driver;
+    CursoMap cursoMap;
 
     public CursosPage(WebDriver driver) {
         this.driver = driver;
+        cursoMap = new CursoMap();
+        PageFactory.initElements(driver, cursoMap);
     }
 
     public String getTitulo2() {
-        String xpathTitulo = "//section[2]//h2";
-        WebElement h2Titulo = driver.findElement(By.xpath(xpathTitulo));
-        return h2Titulo.getText();
+        return cursoMap.h2Titulo.getText();
     }
 }
