f22b8e96526903f80124f70efb16a2a0139d25c7
adamvinicius <adamvinicius@gmail.com>
2021-10-16 10:33:07 -0300

    aula 15 maps e pages

---
+++ src/test/java/br/com/chronosacademy/maps/LoginMap.java
@@ -0,0 +1,14 @@
+package br.com.chronosacademy.maps;
+
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+    public WebElement btnLogin;
+    public WebElement btnFechar;
+    public WebElement divFechaModal;
+    public WebElement inpUserName;
+    public WebElement inpPassword;
+    public WebElement inpRemember;
+    public WebElement btnSignIn;
+    public WebElement linkCreateAccount;
+}
